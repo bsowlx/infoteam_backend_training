64a6a2c2e6b592ffed5fc672806adda69d2a2e92
bsowlx <bsowlx@gmail.com>
2025-10-06 00:44:20 +0900

    Add CreatePostDto and update Post entity

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsNumber, IsString} from "class-validator";
+
+export class CreatePostDto {
+    @IsNumber()
+    @IsNotEmpty()
+    userId: number;
+
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsString()
+    @IsNotEmpty()
+    content: string;
+}
