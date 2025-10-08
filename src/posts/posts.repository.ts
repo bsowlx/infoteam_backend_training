2add37b3f7916a4540f87048ab0c6bf115b98924
bsowlx <bsowlx@gmail.com>
2025-10-09 00:58:18 +0900

    Implement Prisma integration + add posts repository layer

---
+++ src/posts/posts.repository.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { PrismaService } from 'prisma/prisma.service';
+
+@Injectable()
+export class PostsRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createPostDto: CreatePostDto) {
+    return this.prisma.post.create({
+        data: createPostDto,
+    });
+  }
+
+  async findAll() {
+    return this.prisma.post.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.prisma.post.findUnique({
+      where: { id },
+    });
+  }
+
+  async findByUserId(userId: number) {
+    return this.prisma.post.findMany({
+      where: { userId },
+    });
+  }
+
+  async update(id: number, updatePostDto: UpdatePostDto) {
+    return this.prisma.post.update({
+      where: { id },
+      data: updatePostDto,
+    });
+  }
+
+  async remove(id: number) {
+    return this.prisma.post.delete({
+      where: { id },
+    });
+  }
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,80 +1,42 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
-import { Post } from './entities/post.entity';
+import { PostsRepository } from './posts.repository';
 
 @Injectable()
 export class PostsService {
-  private posts: Post[] = [
-    {
-      id: 1,
-      userId: 1,
-      title: '1 - 1',
-      content: 'user 1, id 1',
-    },
-    {
-      id: 2,
-      userId: 1,
-      title: '1 - 2',
-      content: 'user 1, id 2',
-    },
-    {
-      id: 3,
-      userId: 2,
-      title: '2 - 3',
-      content: 'user 2, id 3',
-    },
-  ];
-  create(createPostDto: CreatePostDto) {
-    // new ID generation
-    const newId = this.posts.length > 0 
-      ? Math.max(...this.posts.map(p => p.id)) + 1 
-      : 1;
-    
-    const newPost: Post = {
-      id: newId,
-      ...createPostDto,
-    };
-    
-    this.posts.push(newPost);
-    return newPost;
+  constructor(private postsRepository: PostsRepository) {}
+
+  async create(createPostDto: CreatePostDto) {
+    return this.postsRepository.create(createPostDto);
   }
 
-  findAll() {
-    return this.posts;
+  async findAll() {
+    return this.postsRepository.findAll();
   }
 
-  findByUserId(userId: number) {
-    return this.posts.filter(post => post.userId === userId);
+  async findByUserId(userId: number) {
+    return this.postsRepository.findByUserId(userId);
   }
 
-  findOne(id: number) {
-     const post = this.posts.find(post => post.id === id);
-    if (!post) {
+  async findOne(id: number) {
+    return this.postsRepository.findOne(id);
+  }
+  async findOneOrFail(id: number) {
+    const post = await this.postsRepository.findOne(id);
+    if (!post) {  
       throw new NotFoundException(`Post with ID ${id} not found`);
     }
     return post;
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    const postIndex = this.posts.findIndex(post => post.id === id);
-    if (postIndex === -1) {
-      throw new NotFoundException(`Post with ID ${id} not found`);
-    }
-    this.posts[postIndex] = {
-      ...this.posts[postIndex],
-      ...updatePostDto
-    };
-    return this.posts[postIndex];
+  async update(id: number, updatePostDto: UpdatePostDto) {
+    await this.findOneOrFail(id);
+    return this.postsRepository.update(id, updatePostDto);
   }
 
-  remove(id: number) {
-    const postIndex = this.posts.findIndex(post => post.id === id);
-    if (postIndex === -1) {
-      throw new NotFoundException(`Post with ID ${id} not found`);
-    }
-    const deletedPost = this.posts[postIndex];
-    this.posts.splice(postIndex, 1);
-    return deletedPost;
+  async remove(id: number) {
+    await this.findOneOrFail(id);
+    return this.postsRepository.remove(id);
   }
 }
